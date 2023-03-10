43101052494d6194cf4710e763b3861dc587841e
asd <ads@asdasd>
2023-03-10 13:53:10 +0900

    UnityProject

---
+++ MyUnityProject/Assets/Script/BackGroundController.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundController : MonoBehaviour
+{
+    // BackGround가 모여있는 계층구조의 최상위 객체(부모)
+    private Transform parent;
+
+    // Sprite를 포함하고 있는 Conponenet
+    private SpriteRenderer spriteRenderer;
+    
+    // 이미지
+    private Sprite sprite;
+    
+    // 생성지점
+    private float endPoint;
+    // 삭제 지점
+    private float exitPoint;
+    // 이미지 이동 속도
+    public float Speed;
+
+    // 플레이어 정보
+    private GameObject player;
+    private PlayerController playerController;
+    // 움직임 정보
+    private Vector3 movemane;
+    // 이미지가 중앙 위치에 정상적으로 노출될 수 있도록 하기 위한 완충 역할.
+    private Vector3 offset = new Vector3(0.0f, 7.5f, 0.0f);
+
+    private void Awake()
+    {
+        // 플레이어의 기본정보를 받아온다.
+        player = GameObject.Find("Player").gameObject;
+        // 부모객체를 받아온다.
+        parent = GameObject.Find("BackGround").transform;
+        // 플레이어 이미지를 담고있는 구성요소를 받아온다.
+        playerController = player.GetComponent<PlayerController>();
+        // 현재 이미지를 담고있는 구성요소를 받아온다.
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+       
+    }
+
+    void Start()
+    {
+        // 구성요소에 포함된 이미지를 받아온다.
+        sprite = spriteRenderer.sprite;
+        // 생성지점을 세팅
+        endPoint = sprite.bounds.size.x * 0.5f + transform.position.x;
+        // 삭제지점을 세팅
+        exitPoint = -(sprite.bounds.size.x * 0.5f) + player.transform.position.x;
+    }
+
+    void Update()
+    {
+        
+        // 플레이어가 바라보고있는 방향에 따라 분기된다.
+        if (playerController)
+        {
+
+        }
+        else
+        { 
+            
+        }
+        movemane = Vector3.zero;
+        if (Input.GetAxisRaw("Horizontal") > 0 && player.transform.position.x >= 0)
+        {
+            movemane = new Vector3(
+                Input.GetAxisRaw("Horizontal") * Time.deltaTime * Speed + offset.x,
+                player.transform.position.y + offset.y,
+                0.0f + offset.z);
+        }
+        
+
+
+        // 이동 정보 적용
+        transform.position -= movemane;
+        endPoint -= movemane.x;
+
+        // 동일 이미지 복사
+        if (player.transform.position.x + sprite.bounds.size.x * 0.5f + 1 > endPoint)
+        {
+            // 복사 이미지 생성
+            GameObject Obj = Instantiate(this.gameObject);
+
+            // 복제된 이미지의 부모를 설정한다.
+            Obj.transform.parent = parent.transform;
+            // 복제된 이미지의 이름을 설정한다.
+            Obj.transform.name = transform.name;
+
+            // 복제된 이미지의 위치 설정
+            Obj.transform.position = new Vector3(
+                endPoint + sprite.bounds.size.x * 0.5f,
+                transform.position.y,
+                0.0f);
+
+            // 생성지점을 변경한다.
+            endPoint += endPoint + sprite.bounds.size.x * 0.5f;
+        }
+        // 삭제지점 도달 시 이미지 삭제.
+        if (transform.position.x + (sprite.bounds.size.x * 0.5f) - 2 < exitPoint)
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
---
+++ MyUnityProject/Assets/Script/BulletController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    // 총알 속도
+    private float Speed;
+
+    // 총알이 충돌한 횟수
+    private float hp;
+
+    // 이펙트효과 원본
+    public GameObject fxPrefab;
+
+    // 총알이 가야할 방향
+    public Vector3 Direction { get; set;} //과 동일
+   
+    private void Start()
+    {
+        // 속도 초기값
+        Speed = 10.0f;
+        // 총알의 최대 충돌 횟수 지정
+        hp = 1;
+    }
+    void Update()
+    {
+        // 방향으로 속도만큼 위치를 변경
+        transform.position += Direction * Speed * Time.deltaTime;
+    }
+
+    // 충돌체와 물리엔진이 포함된 오브젝트가 다른 충돌체와 충돌한다면 실행되는 함수.
+
+    // Enter : 충돌된 순간 프레임 
+    // Stay : 충돌된 다음 프레임부터 Exit프레임 직전까지
+    // Exit : 충돌이 끝난 후 그 다음 프레임
+
+    // Trigger : 충돌 판정만하고 충돌체를 통과한다.
+    // Collision : 충돌 판정을하고 충돌체와 충돌한다.
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 충돌 횟수 차감.
+        --hp;
+        
+
+        if (collision.tag == ("wall"))
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else if (hp == 0) // 총알의 충돌 횟수가 0 되면 총알 삭제.
+           Destroy(this.gameObject);
+           
+
+        // 이펙트효과 복제
+        GameObject Obj = Instantiate(fxPrefab);
+
+        // 진동효과를 담당하는 관리자 생성
+        GameObject camera = new GameObject("Camera Test");
+
+        // 진동효과 생성
+        camera.AddComponent<CameraController>();
+
+        // 이펙트 효과의 위치를 지정
+        Obj.transform.position = transform.position;
+
+        // 충돌한 대상을 삭제
+        Destroy(collision.transform.gameObject);     
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        print("Stay");
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        print("Exit");
+    }
+}
